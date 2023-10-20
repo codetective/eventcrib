969e1e0eb75382e528fdee80d25646e413fe2245
codetective <iaminiodu@gmail.com>
2023-10-20 12:41:29 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     WALLET_CONNECT_PROJECT_ID: process.env.WALLET_CONNECT_PROJECT_ID,
     WEB3STORAGE_TOKEN: process.env.WEB3STORAGE_TOKEN,
     DATABASE_URL: process.env.DATBASE_URL,
+    WEB3STORAGE_TOKEN: process.env.WEB3STORAGE_TOKEN,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   experimental: {
     serverActions: true,
